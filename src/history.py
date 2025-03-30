610344c8cad59ff45cbbc93c0bb83a77ae457595
TAFH-debug <talim2007@mail.ru>
2025-03-30 14:27:02 +0500

    commit

--- src/history.py
+++ src/history.py
@@ -1,12 +1,46 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from sqlmodel import select
 from google.genai import types
 
+from .prompts import DOCS
+from .models.user import User
+from .authService import Payload, get_payload
 from .models.history import History, HistoryCreate
 from .db import SessionDep
 from .gemini import client
 
 history_router = APIRouter(prefix="/history")
+
+FEEDBACK_PROMPT = """
+Ты тренер по подготовке полицейских. 
+Твоя задача помочь пользователю проанализировать переговоры. 
+Выдай фидбэк в формате:
+{
+    "analysis": "<анализ>",
+    "cons": ["недостаток"],
+    "pros": ["достойнство"],
+}
+Вот материалы для переговоров:
+""" + DOCS
+
+@history_router.get("/feedback/{id}")
+async def get_docs(id: int, session: SessionDep):
+    msg = session.exec(select(History).where(History.id == id)).first().messages
+    return client.models.generate_content(model="gemini-2.0-flash", 
+                                          contents=msg,
+                                          config=types.GenerateContentConfig(
+            system_instruction=FEEDBACK_PROMPT,
+            response_mime_type="application/json"
+        )).text
+
+@history_router.get("/me")
+async def get_me(session: SessionDep, payload: Payload = Depends(get_payload)):
+    user = session.exec(select(User).where(User.id == payload.id)).first()
+    history = []
+    for i in user.get_history():
+        history.append(session.exec(select(History).where(History.id == i)).first())
+    return history
+
 
 @history_router.get("/")
 async def get_histories(session: SessionDep):
@@ -24,11 +58,3 @@ async def create_history(history: HistoryCreate, session: SessionDep):
     session.commit()
     session.refresh(db_history)
     return db_history
-
-@history_router.get("/{id}")
-async def get_recommendations(id: int):
-    return client.models.generate_content("gemini-2.0-flash", 
-                                          contents="",
-                                          config=types.GenerateContentConfig(
-            system_instruction="Ты тренер по подготовке полицейских. Твоя задача помочь пользователю проанализировать переговоры. Не форматируй ответ. Вот материалы для переговоров: \n" + DOCS
-        ))
